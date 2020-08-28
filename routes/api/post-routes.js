7162a5de4757d0321b3efa21d1ee4b278010563e
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-08-27 21:00:21 -0500

    added comment functionality

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 //get all posts
 router.get('/', (req, res) => {
     console.log('=========================');
     Post.findAll({
+        order: [['created_at', 'DESC']],
         //Query configuration
         attributes: [
             'id', 
@@ -14,12 +15,19 @@ router.get('/', (req, res) => {
             'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-        order: [['created_at', 'DESC']],
         include: [
             {
-                model: User,
-                attributes: ['username']
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'], 
+                include: {
+                    model: User,
+                    attributes: ['username']
             }
+        },
+        {
+            model: User,
+            attributes: ['username']
+        }
         ]
        })
        .then(dbPostData => res.json(dbPostData))
@@ -42,6 +50,14 @@ router.get('/:id', (req, res) => {
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'], 
+                include: {
+                    model: User,
+                    attributes: ['username']
+            }
+        },
             { 
                 model: User,
                 attributes: ['username']
