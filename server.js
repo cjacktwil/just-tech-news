365faf00436e7182cbfa889b1040b08ed7c08c28
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-09-01 16:30:37 -0500

    added helper functions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
@@ -7,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const session = require('express-session');
 
